8ee100296dc5d2acfc4b429b6cfb374f88551c5f
Jeeva523-R <jeevananthravikumar@gmail.com>
2026-04-16 10:42:46 +0530

    Billing authentication

---
+++ billing/server.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+connectDB();
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+app.use(express.json());
+
+// 🔥 ROUTES
+app.use("/sales", require("./routes/saleRoutes"));
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => {
+  console.log(`Billing Service running on port ${PORT}`);
+});
